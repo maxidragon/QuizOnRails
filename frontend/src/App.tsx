11f15b68c45c0b7df3753ab1c2111e45932d7303
maxidragon <mgwspinanie2006@wp.pl>
2023-09-19 18:34:14 +0200

    Add frontend auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,39 @@
 import "./App.css";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
+import { ConfirmProvider } from "material-ui-confirm";
+import Register from "./Pages/Auth/Register/Register";
+import Login from "./Pages/Auth/Login/Login";
+
+
+const router = createHashRouter([
+  {
+    path: "/auth/login",
+    element: <Login />,
+  },
+  {
+    path: "/auth/register",
+    element: <Register />,
+  },
+]);
+
+const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
 
 function App() {
-  return <>QuizOnRails</>;
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <SnackbarProvider>
+        <ConfirmProvider>
+          <RouterProvider router={router} />
+        </ConfirmProvider>
+      </SnackbarProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ frontend/src/logic/auth.ts
@@ -0,0 +1,50 @@
+import { backendRequest } from "./request";
+
+export const registerUser = async (
+  email: FormDataEntryValue | null,
+  username: FormDataEntryValue | null,
+  password: FormDataEntryValue | null
+) => {
+  try {
+    const body = {
+      user: {
+        email: email,
+        name: username,
+        password: password,
+      },
+    };
+    const response = await backendRequest("signup", "POST", false, body);
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const login = async (
+  email: FormDataEntryValue | null,
+  password: FormDataEntryValue | null
+) => {
+  try {
+    const response = await backendRequest("login", "POST", false, {
+      user: {
+        email: email,
+        password: password,
+      },
+    });
+    if (response.status === 200) {
+      const headers: [string, string][] = [...response.headers];
+      const authorizationHeader = headers.find(
+        (header) => header[0] === "authorization"
+      );
+      if (!authorizationHeader) throw new Error("No authorization header");
+      const token = authorizationHeader[1].split(" ")[1];
+      localStorage.setItem("token", token);
+      return response.status;
+    }
+    return response.status;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const logout = async () => {
+  localStorage.removeItem("token");
+};
